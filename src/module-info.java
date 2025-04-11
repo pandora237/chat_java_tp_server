1d7eb11cd00f87f8c165fd99bb321323aa64d489
Franky Alongamo <alongamofranky@gmail.com>
2025-04-11 14:14:42 +0200

    update to fix javaFx libs

--- src/module-info.java
+++ src/module-info.java
@@ -10,6 +10,7 @@ module chat_java_tp_server_ {
 	requires javafx.graphics;
 	requires javafx.controls; 
 	requires java.base;
+	 requires javafx.fxml; 
 	requires org.json;
 	requires java.desktop;  
 
